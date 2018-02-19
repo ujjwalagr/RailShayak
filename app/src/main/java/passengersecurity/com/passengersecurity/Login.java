61e1d0f49d152b2b255e302a73f551ff3b8b4106
Shivam Shukla <developershivamshukla@gmail.com>
2018-02-19 10:12:48 +0530

    updates

--- app/src/main/java/passengersecurity/com/passengersecurity/Login.java
+++ app/src/main/java/passengersecurity/com/passengersecurity/Login.java
@@ -1,54 +1,71 @@
 package passengersecurity.com.passengersecurity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     static String s1, s2, s3, s4;
-    Button b, b1;
-    EditText e1, e2, e3, e4;
+    TextView b1;
+    EditText e1,e2,e3,e4;
+    SharedPreferences f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
-        e1 = findViewById(R.id.nametxt);
-        e2 = findViewById(R.id.pnrtxt);
-        e3 = findViewById(R.id.emailtxt);
-        e4 = findViewById(R.id.phonetxt);
-        b = findViewById(R.id.vb);
-        b1 = findViewById(R.id.rb);
-        b1.setEnabled(false);
-        b.setOnClickListener(new View.OnClickListener() {
+        b1=findViewById(R.id.sb);
+        e1=findViewById(R.id.nametxt);
+        e2=findViewById(R.id.pnrtxt);
+        e3=findViewById(R.id.emailtxt);
+        e4=findViewById(R.id.phonetxt);
+        f=getSharedPreferences("sp",MODE_PRIVATE);
+        //b=findViewById(R.id.vb);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //b1.setEnabled(false);
+                SharedPreferences.Editor e = f.edit();
                 s1 = e1.getText().toString();
                 s2 = e2.getText().toString();
                 s3 = e3.getText().toString();
-                s4 = e4.getText().toString();
+                s4=e4.getText().toString();
 
-                if (s1.isEmpty())
+                if(s1.isEmpty())
                     Toast.makeText(Login.this, "Please Enter Name", Toast.LENGTH_SHORT).show();
-                else if (s2.isEmpty() || s2.length() != 10)
+                else if(s2.isEmpty()||s2.length()!=10||!android.util.Patterns.PHONE.matcher(s4).matches())
                     Toast.makeText(Login.this, "Please Enter valid PNR", Toast.LENGTH_SHORT).show();
-                else if (s3.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(s3).matches())
+                else if(s3.isEmpty()|| !android.util.Patterns.EMAIL_ADDRESS.matcher(s3).matches())
                     Toast.makeText(Login.this, "Please Enter The Valid Email", Toast.LENGTH_SHORT).show();
-                else if ((s4.isEmpty()) || (s4.length() != 10) || !android.util.Patterns.PHONE.matcher(s4).matches())
+                else if((s4.isEmpty())||(s4.length()!=10)|| !android.util.Patterns.PHONE.matcher(s4).matches())
                     Toast.makeText(Login.this, "InValid Phone No.", Toast.LENGTH_SHORT).show();
+                else if(s1.equals("admin")&& s2.equals("1234567890")&& s3.equals("coder@g.com")&&s4.equals("1234567890")) {
+                    //b1.setEnabled(true);
+                    e.putString("k1",s1);
+                    e.putString("k2",s2);
+                    e.putString("k3",s3);
+                    e.putString("k4",s4);
+                    e.commit();
+
+                    b1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent i = new Intent(Login.this, Mainpage.class);
+                            startActivity(i);
+                            Toast.makeText(Login.this, "Successful Register", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
                 else
-                    b1.setEnabled(true);
-                b1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent i = new Intent(Login.this, Mainpage.class);
-                        startActivity(i);
-                    }
-                });
+                {
+                    Toast.makeText(Login.this, "Invalid Details", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
--- app/src/main/java/passengersecurity/com/passengersecurity/SplashScreen.java
+++ app/src/main/java/passengersecurity/com/passengersecurity/SplashScreen.java
@@ -1,15 +1,24 @@
 package passengersecurity.com.passengersecurity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
+    SharedPreferences d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        d=getSharedPreferences("sp",MODE_PRIVATE);
+        final String name = d.getString("k1",null);
+        final String pnr = d.getString("k2",null);
+        final String email = d.getString("k3",null);
+        final String phone = d.getString("k4",null);
+        Toast.makeText(this, "Name="+name+"\n"+"Pnr="+pnr+"\n"+"Email="+email+"\n"+"Phone"+phone+"\n", Toast.LENGTH_SHORT).show();
         Thread th = new Thread() {
             @Override
             public void run() {
@@ -17,8 +26,17 @@ public class SplashScreen extends AppCompatActivity {
                     sleep(3000);
                 } catch (Exception h) {
                 } finally {
-                    Intent i = new Intent(SplashScreen.this, FirstPage.class);
-                    startActivity(i);
+                    if(name!=null && pnr!=null && email!=null && phone!=null)
+                    {
+                        Intent i = new Intent(SplashScreen.this, Mainpage.class);
+                        startActivity(i);
+                    }
+                    else
+                    {
+                        Intent i = new Intent(SplashScreen.this,FirstPage.class);
+                        startActivity(i);
+                    }
+
                     finish();
                 }
             }
