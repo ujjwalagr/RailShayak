48d6f30ead75bbdb2602c4ff6d57d1d7e9138055
Shivam Shukla <developershivamshukla@gmail.com>
2018-03-24 22:19:08 +0530

    Revert Changes

---
+++ app/src/main/java/passengersecurity/com/passengersecurity/GoogleLogin.java
@@ -0,0 +1,17 @@
+package passengersecurity.com.passengersecurity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class GoogleLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_login);
+    }
+}
